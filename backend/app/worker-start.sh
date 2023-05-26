44d5905e3e13c9b9358c643f1f6dbd6d23d22ba8
miladaleali <miladaleali1392@gmail.com>
2023-05-26 19:12:33 +0000

    REFACTOR: worker-start to run celery command directly

--- backend/app/worker-start.sh
+++ backend/app/worker-start.sh
@@ -2,4 +2,6 @@
 python /app/app/celeryworker_pre_start.py
 
 # celery worker -A app.worker -l info -Q main-queue -c 1
-python manage.py run celery
+# python manage.py run celery
+# argv = ['worker', '--pool=gevent', '-lINFO', '-Qdata,signal,database,position,engine,starter', '--concurrency=10000']
+celery -A pedesis.tasks_manage:manager worker --pool=gevent -l INFO -Q data,signal,database,position,engine,starter --concurrency=10000
